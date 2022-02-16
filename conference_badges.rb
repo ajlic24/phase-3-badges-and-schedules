a7a0ecf7313a0c2eeea7987cae092bc6c9237745
ajlic24@gmail.com <ajlic24@gmail.com>
2022-02-16 18:12:51 -0500

    commit tree 80a486c50f867ade8abdf6b6ad0081d690f64fa7

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator name_arr
+    name_arr.map do |name|
+        badge_maker name
+    end
+end
+
+def assign_rooms name_arr
+    name_arr.map.with_index do |speaker, i| 
+        "Hello, #{speaker}! You'll be assigned to room #{i+1}!"
+    end
+end
+
+def printer name_arr
+    batch_badge_creator(name_arr).each do |person|
+        puts person
+    end
+    assign_rooms(name_arr).each {|person| puts person}
+    
+end
